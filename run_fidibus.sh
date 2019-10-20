b01283056d9aeff5e6f717f2003b00f0ce2d551a
Tim <46388504+timlai4@users.noreply.github.com>
2019-10-19 21:16:16 -0400

    Tracking successful runs

--- run_fidibus.sh
+++ run_fidibus.sh
@@ -7,6 +7,9 @@ species_list=($(cat "$batch"))
 
 for species in ${species_list[@]}
 do
-    singularity exec -e -B `pwd` aegean.simg fidibus -c=. --refr=$species download prep iloci breakdown stats
+    if singularity exec -e -B `pwd` aegean.simg fidibus -c=. --refr=$species download prep iloci breakdown stats
+    then
+        echo $species >> final_${batch}
+    fi
 done
 find species -type f -not -name "*.gff3" -and -not -name "*.tsv" -exec rm {} \;
