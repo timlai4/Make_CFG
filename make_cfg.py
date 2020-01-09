8c35785d5945a3a6fa8800250aa852d8b02cfac7
Tim Lai <tlai4@disroot.org>
2020-01-09 01:00:49 -0500

    Handle exceptions

--- make_cfg.py
+++ make_cfg.py
@@ -8,18 +8,31 @@ Created on Sun Sep 29 22:00:07 2019
 import re
 
 batch = [] # Initiate a batch txt file at the end to facilitate fidibus
-branch = 'invertebrate' 
-p = re.compile("\d{3}.\d_")
-f = open(branch + '_latest.txt', 'r')
+branch = 'invertebrate' # Change this
+p = re.compile("\d{3}.\d_") # Accessions of the form GCF_(digits).(digit) for the most part.
+# Exceptions: GCF_(digits).(two digits)
+p2 = re.compile("\d{3}.\d{2}_")  # Handles this exception
+f = open(branch + '_latest.txt', 'r') 
 for line in f:
     yaml = {}
     entry = line.strip().split('/')
     name = entry[1].split('_')
     info = entry[-1]
     match = p.search(info)
-
-    yaml['file_name'] = name[0][0] + name[1][:3]
-    yaml['species'] = name[0] + ' ' + name[1]
+    if match is None: # Handling the above exception with p2
+        match = p2.search(info)
+    try:
+        fname = name[0][0] + name[1][:3]
+    except IndexError:
+        fname = name[0][:4]
+    if len(fname) == 4:
+        yaml['file_name'] = fname
+    else:
+        continue
+    try:
+        yaml['species'] = name[0] + ' ' + name[1]
+    except IndexError:
+        yaml['species'] = name[0]
     yaml['accession'] = info[:match.start()+5]
     yaml['build'] = info[match.start()+6:]
     L = [yaml['file_name'] + ':\n', "    species: " + yaml['species'] + '\n',
@@ -27,7 +40,7 @@ for line in f:
          "    accession: " + yaml['accession'] + '\n',
          "    build: " + yaml['build']]
     batch.append(yaml['file_name'] + '\n')
-    with open(yaml['file_name'] + ".yml",'w') as y:
+    with open('viral/' + yaml['file_name'] + ".yml",'w') as y:
         y.writelines(L) 
 f.close()
 
