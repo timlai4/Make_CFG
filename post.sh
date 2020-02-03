42a2c0d01e206a96c1da2b6089e03123e4df41d7
Tim <46388504+timlai4@users.noreply.github.com>
2020-02-03 16:08:20 -0500

    Added gqnt

--- post.sh
+++ post.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -u
 
-batch=final_viral.txt
+batch=final_fungi.txt
 species_list=($(cat "$batch"))
 
-singularity exec -e -B `pwd` aegean.simg fidibus-ilocus-summary.py --outfmt=tsv ${species_list[@]} > viral-ilocus-summaries.tsv
-singularity exec -e -B `pwd` aegean.simg fidibus-pilocus-summary.py --outfmt=tsv ${species_list[@]} > viral-pilocus-summaries.tsv
-singularity exec -e -B `pwd` aegean.simg fidibus-milocus-summary.py --outfmt=tsv ${species_list[@]} > viral-milocus-summaries.tsv
-singularity exec -e -B `pwd` aegean.simg fidibus-compact.py --centroid=2.25  --length=1000000 --iqnt=0.95 ${species_list[@]} > phisigma_viral.tsv
+singularity exec -e -B `pwd` aegean.simg fidibus-ilocus-summary.py --outfmt=tsv ${species_list[@]} > fungi-ilocus-summaries.tsv
+singularity exec -e -B `pwd` aegean.simg fidibus-pilocus-summary.py --outfmt=tsv ${species_list[@]} > fungi-pilocus-summaries.tsv
+singularity exec -e -B `pwd` aegean.simg fidibus-milocus-summary.py --outfmt=tsv ${species_list[@]} > fungi-milocus-summaries.tsv
+singularity exec -e -B `pwd` aegean.simg fidibus-compact.py --centroid=2.25  --length=1000000 --iqnt=0.95 --gqnt=0.05 ${species_list[@]} > phisigma_fungi.tsv
